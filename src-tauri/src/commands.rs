a5e92ba3d3cc6ae78a77f44d7807a7b35e8977fc
lowkeylone <xavier.saliniere@pm.me>
2021-08-30 00:06:32 +0200

    style: remove useless import

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,7 +1,6 @@
 use tauri::{command};
 use crate::mm_session::MMSession;
 use crate::error::Error;
-use serde_json::Value;
 
 #[command]
 pub(crate) fn request_new_session() -> Result<MMSession, Error> {
